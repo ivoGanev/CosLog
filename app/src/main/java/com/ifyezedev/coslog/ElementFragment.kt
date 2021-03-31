e7bfd55e593f3556a86b4a7733bc6f88a191a4b9
Ivo Ganev <ivo.ganev@yahoo.com>
2021-03-31 12:58:25 +0000

    Add ToBuyFragment and click listener for element fragment

--- app/src/main/java/com/ifyezedev/coslog/ElementFragment.kt
+++ app/src/main/java/com/ifyezedev/coslog/ElementFragment.kt
@@ -2,19 +2,24 @@ package com.ifyezedev.coslog
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.commit
+import androidx.navigation.fragment.NavHostFragment
 import com.google.android.material.tabs.TabLayoutMediator
 import com.ifyezedev.coslog.databinding.FragmentElementBinding
 import java.lang.IllegalArgumentException
 
 
-class ElementFragment : BindingFragment<FragmentElementBinding>() {
-    private lateinit var elementsFragmentStateAdapter: ElementsFragmentStateAdapter
+class ElementFragment : BindingFragment<FragmentElementBinding>(), View.OnClickListener {
     override fun bindingLayoutId() = R.layout.fragment_element
+
+    private lateinit var elementsFragmentStateAdapter: ElementsFragmentStateAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         with(binding) {
+            binding.fab.setOnClickListener(this@ElementFragment)
             elementsFragmentStateAdapter = ElementsFragmentStateAdapter(this@ElementFragment)
             elementsViewPager.adapter = elementsFragmentStateAdapter
+
             TabLayoutMediator(elementsTabLayout, elementsViewPager) { tab, position ->
                 tab.text = when (position) {
                     0 -> "To Buy"
@@ -23,6 +28,15 @@ class ElementFragment : BindingFragment<FragmentElementBinding>() {
                 }
             }.attach()
         }
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.fab -> onFabClicked()
+        }
+    }
+
+    private fun onFabClicked() {
     }
 }
 
---
+++ app/src/main/java/com/ifyezedev/coslog/ToBuyFragment.kt
@@ -0,0 +1,13 @@
+package com.ifyezedev.coslog
+
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.Navigation
+import com.ifyezedev.coslog.databinding.FragmentToBuyBinding
+
+class ToBuyFragment : BindingFragment<FragmentToBuyBinding>() {
+    override fun bindingLayoutId(): Int = R.layout.fragment_to_buy
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    }
+}
