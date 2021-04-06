32eda271924afa5d21730fba728a21775a9bf3f0
albertogfv <albertogfv@gmail.com>
2021-04-05 19:19:35 -0700

    added database and dao functions

--- app/src/main/java/com/ifyezedev/coslog/data/db/CosLogDao.kt
+++ app/src/main/java/com/ifyezedev/coslog/data/db/CosLogDao.kt
@@ -1,4 +1,54 @@
 package com.ifyezedev.coslog.data.db
 
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Transaction
+import com.ifyezedev.coslog.data.db.entities.*
+import com.ifyezedev.coslog.data.db.entities.relations.*
+
 interface CosLogDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCosplay(cosplay: Cosplay)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertElement(element: Element)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertEvent(event: Event)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNote(note: Note)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertPicture(picture: Picture)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertProcess(task: Task)
+
+    @Transaction
+    @Query("SELECT * FROM cosplay_items WHERE cid = :cid")
+    suspend fun getCosplayAndElementWithCid(cid: Int): List<CosplayAndElement>
+
+    @Transaction
+    @Query("SELECT * FROM cosplay_items WHERE cid = :cid")
+    suspend fun getCosplayAndEventWithCid(cid: Int): List<CosplayAndEvent>
+
+    @Transaction
+    @Query("SELECT * FROM cosplay_items WHERE cid = :cid")
+    suspend fun getCosplayAndNoteWithCid(cid: Int): List<CosplayAndNote>
+
+    @Transaction
+    @Query("SELECT * FROM cosplay_items WHERE cid = :cid")
+    suspend fun getCosplayAndPictureWithCid(cid: Int): List<CosplayAndPicture>
+
+    @Transaction
+    @Query("SELECT * FROM cosplay_items WHERE cid = :cid")
+    suspend fun getCosplayAndProcessWithCid(cid: Int): List<CosplayAndProcess>
+
+    @Transaction
+    @Query("SELECT * FROM cosplay_items WHERE cid = :cid")
+    suspend fun getCosplayAndTaskWithCid(cid: Int): List<CosplayAndTask>
+
 }
--- app/src/main/java/com/ifyezedev/coslog/data/db/CosLogDatabase.kt
+++ app/src/main/java/com/ifyezedev/coslog/data/db/CosLogDatabase.kt
@@ -1,17 +1,46 @@
 package com.ifyezedev.coslog.data.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.ifyezedev.coslog.data.db.entities.Cosplay
+import com.ifyezedev.coslog.data.db.entities.*
 
 
 @Database(
-        entities = [Cosplay::class],
+        entities = [
+                Cosplay::class,
+                Element::class,
+                Event::class,
+                Note::class,
+                Picture::class,
+                Process::class,
+                Task::class
+        ],
         version = 1
 
 )
 abstract class CosLogDatabase : RoomDatabase(){
 
         abstract fun getCosLogDao() : CosLogDao
+
+        companion object {
+                @Volatile
+                private var INSTANCE: CosLogDatabase? = null
+
+                fun getInstance(context: Context): CosLogDatabase {
+                        synchronized(this){
+                                return INSTANCE ?: Room.databaseBuilder(
+                                        context.applicationContext,
+                                        CosLogDatabase::class.java,
+                                        "cosplay_db"
+                                ).build().also{
+                                        INSTANCE = it
+                                }
+                        }
+                }
+
+
+        }
 
 }
