87d1b7303bbac2eeee2f3f011e37e9a7f887070b
Ivo Ganev <ivo.ganev@yahoo.com>
2021-04-07 13:41:20 +0000

    feature: Add images in coslog small picture viewer

--- app/src/main/java/com/ifyezedev/coslog/ToBuyFragment.kt
+++ app/src/main/java/com/ifyezedev/coslog/ToBuyFragment.kt
@@ -1,17 +1,30 @@
 package com.ifyezedev.coslog
 
+import android.app.Activity.RESULT_OK
+import android.content.ClipData
+import android.content.ContentResolver
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
 import android.view.*
+import androidx.recyclerview.widget.RecyclerView
 import com.ifyezedev.coslog.core.builders.buildIntent
 import com.ifyezedev.coslog.databinding.FragmentToBuyBinding
+import com.ifyezedev.coslog.databinding.PictureItemBinding
+
 
 class ToBuyFragment : CosplayBaseFragment<FragmentToBuyBinding>(), View.OnClickListener {
+
     override fun bindingLayoutId(): Int = R.layout.fragment_to_buy
+
+    private val adapterData: MutableList<Bitmap> = mutableListOf()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.bottom.buttonAddImage.setOnClickListener(this)
+        binding.bottom.recyclerView.adapter = Adapter(adapterData)
     }
 
     override fun onClick(v: View?) {
@@ -21,9 +34,69 @@ class ToBuyFragment : CosplayBaseFragment<FragmentToBuyBinding>(), View.OnClickL
     }
 
     private fun onAddImage() {
-        val intent = buildIntent(Intent.ACTION_PICK) {
+        val intent = buildIntent() {
+            action = Intent.ACTION_GET_CONTENT
             type = "image/*"
+            putExtra("return-data", true);
+            putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
         }
-        startActivityForResult(intent, 1)
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), 0)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
+        if (resultCode == RESULT_OK && requestCode == 0) {
+            // Handle single files
+            val imagesUri: MutableList<Uri> = mutableListOf()
+            intent?.let {
+            }
+            intent?.data?.let { uri -> imagesUri.add(uri) }
+            intent?.clipData?.let { clipData -> imagesUri.addAll(clipData.mapUri()) }
+            val bitmapImages = requireContext()
+                .contentResolver
+                .openInputStream(imagesUri)
+
+            adapterData.addAll(bitmapImages)
+            binding.bottom.recyclerView.adapter?.notifyDataSetChanged()
+        }
+    }
+
+    private class Adapter(private val data: List<Any>) :
+        RecyclerView.Adapter<Adapter.ViewHolder>() {
+        class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            val binding: PictureItemBinding = PictureItemBinding.bind(itemView)
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.picture_item, parent, false)
+            return ViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder.binding.imageView.setImageBitmap(data[position] as Bitmap?)
+        }
+
+        override fun getItemCount(): Int {
+            return data.size
+        }
+    }
+
+    private fun ContentResolver.openInputStream(uriList: List<Uri>): List<Bitmap> {
+        val bitmapResult = mutableListOf<Bitmap>()
+        for (i in uriList.indices) {
+            val bitmapStream = this.openInputStream(uriList[i])
+            val bitmap = BitmapFactory.decodeStream(bitmapStream)
+            bitmapResult.add(bitmap)
+        }
+        return bitmapResult
+    }
+
+    private fun ClipData.mapUri(): MutableList<Uri> {
+        val result = mutableListOf<Uri>()
+        for (i in 0 until this.itemCount) {
+            result.add(this.getItemAt(i).uri)
+        }
+        return result
     }
 }
+
